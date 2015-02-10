819c7e96e2e8e9db5fc1c6e83bec8c0bef49521f
Vegar Guldal <vegar@guldal.no>
2015-02-10 23:34:40 +0100

    Complete the Roll-a-Ball tutorial
    Tutorial at http://unity3d.com/learn/tutorials/projects/roll-a-ball/

---
+++ roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start() {
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win!";
+		}
+	}
+}
